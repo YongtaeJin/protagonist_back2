e5bdde81a8574c698ebf00caffc7e41d30baf6f0
Yongtae Jin <freview@.nate.com>
2022-12-14 15:04:57 +0900

    Vue SSR 적용

--- server/server.js
+++ server/server.js
@@ -1,3 +1,4 @@
+const fs = require('fs');
 require('dotenv').config();
 const express = require('express');
 const http = require('http');
@@ -10,6 +11,32 @@ const webServer = http.createServer(app);
 
 // 정적 폴더
 app.use(express.static(path.join(__dirname, "../dist")));
+
+// Vue SSR
+const { createBundleRenderer } = require('vue-server-renderer');
+const template = fs.readFileSync(path.join(__dirname, 'index.template.html'), 'utf-8');
+const serverBundle = require(path.join(__dirname, '../dist/vue-ssr-server-bundle.json'));
+const clientManifest = require(path.join(__dirname, '../dist/vue-ssr-client-manifest.json'));
+
+const renderer = createBundleRenderer(serverBundle, {
+	runInNewContext : false,
+	template,
+	clientManifest,
+});
+
+app.get('*', (req, res) => {
+	const ctx = {
+		url : req.url,
+		title : 'Vue SSR App',
+		metas : `<!-- inject more metas -->`,
+	};
+
+	const stream = renderer.renderToStream(ctx);
+
+	stream.on('end', ()=> {
+		console.log('스트림 렌더 종료');
+	}).pipe(res);
+});
 
 // 서버 리슨
 webServer.listen(port, () => {
