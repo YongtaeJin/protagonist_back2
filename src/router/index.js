62ee581ce9e6514ea98e2a847e1032f6c9ba909d
Yongtae Jin <freview@.nate.com>
2022-12-16 16:12:00 +0900

    022 - 회원 Table 생성과 회원가입 레이아웃

--- src/router/index.js
+++ src/router/index.js
@@ -1,43 +1,23 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-import HomeView from '../views/HomeView.vue'
+import routes from './routes';
 
 Vue.use(VueRouter)
 
-const routes = [
-  {
-    path: '/',
-    name: 'home',
-    component: HomeView
-  },
-  {
-    path: '/about',
-    name: 'about',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/AboutView.vue')
-  },
-  {
-    path: '*',
-    name: 'Error',
-    component: () => import(/* webpackChunkName: "error" */ '../views/Error.vue')
-  },
-]
 
 export function createRouter() {
-	const router = new VueRouter({
-		mode: 'history',
-		base: process.env.BASE_URL,
-		routes
-	});
+        const router = new VueRouter({
+                mode: 'history',
+                base: process.env.BASE_URL,
+                routes
+        });
 
   router.beforeEach((to, from, next)=> {
-		next();
-	});
+                next();
+        });
 
-	router.afterEach((to, from)=> {		
-	});
+        router.afterEach((to, from)=> {
+        });
 
-	return router;
+        return router;
 }
---
+++ src/router/routes.js
@@ -0,0 +1,31 @@
+import HomeView from '../views/HomeView.vue'
+
+const routes = [
+    {
+      path: '/',
+      name: 'home',
+      component: HomeView
+    },
+    {
+      path: '/about',
+      name: 'about',
+      component: () => import(/* webpackChunkName: "about" */ '../views/AboutView.vue')
+    },
+    {
+        path: '/login',
+        name: 'NoAuthLogin',
+        component: () => import(/* webpackChunkName: "login" */ '../views/member/Login.vue')
+    },
+    {
+        path: '/join',
+        name: 'NoAuthJoin',
+        component: () => import(/* webpackChunkName: "join" */ '../views/member/Join.vue')
+    },
+    {
+      path: '*',
+      name: 'Error',
+      component: () => import(/* webpackChunkName: "error" */ '../views/Error.vue')
+    },
+  ]
+
+export default routes;
