e5d6455f59691c5e4d43a9b3637cb086dd6b860f
Yongtae Jin <freview@.nate.com>
2022-12-19 15:10:38 +0900

    024 - 중복검사 컴포넌트2 중복검사하기

--- src/components/auth/SingUpForm.vue
+++ src/components/auth/SingUpForm.vue
@@ -1,12 +1,24 @@
 <template>
   <v-form @submit.prevent="save" ref="form" v-model="valid" lazy-validation>
+    <input-duplicate-check ref="id" v-model="form.mb_id" label="아이디" prepend-icon="mdi-account" counter="30" :rules="rules.id()" :cbCheck="cbCheckId" 
+      
+    />
     <v-btn type="submit" block color="primary">회원가입</v-btn>
   </v-form>
 </template>
 
 <script>
+import validateRules from "../../../util/validateRules";
+import InputDuplicateCheck from "../InputForms/InputDuplicateCheck.vue";
 export default {
+  components: { InputDuplicateCheck },
   name: "SignUpForm",
+  props : {
+    cbCheckId : {
+      type : Function,
+      default : null,
+    },
+  },
   data() {
     return {
       valid: true,
@@ -24,8 +36,15 @@ export default {
       },
     };
   },
+  computed : {
+    rules  : () => validateRules,
+  },
   methods: {
-    save() {
+    async save() {
+      this.$refs.form.validate();
+      await this.$nextTick();
+      if(!this.valid) return;
+      if(!this.$refs.id.validate()) return;
       console.log(this.form);
     },
   },
