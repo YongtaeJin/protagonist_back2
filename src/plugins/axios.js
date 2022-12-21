58b2ce890c9a13598b806b5cb9fcd94ea0e293fd
Yongtae Jin <freview@.nate.com>
2022-12-21 10:43:05 +0900

    040 - 무중단 서비스

--- src/plugins/axios.js
+++ src/plugins/axios.js
@@ -9,76 +9,61 @@ import axios from "axios";
 // axios.defaults.headers.post['Content-Type'] = 'application/x-www-form-urlencoded';
 
 let config = {
-  baseURL: process.env.baseURL || process.env.apiUrl || "",
-  timeout: 60 * 1000, // Timeout
-  proxy : {
-		host : 'localhost',
-		port : process.env.VUE_APP_SERVER_PORT,
+	baseURL: process.env.BASE_URL || process.env.apiUrl || "",
+	timeout: 60 * 1000, // Timeout
+	proxy: {
+		host: 'localhost',
+		port: process.env.VUE_APP_SERVER_PORT
 	}
-  //withCredentials: true, // Check cross-site Access-Control
+	// withCredentials: true, // Check cross-site Access-Control
 };
 
 const _axios = axios.create(config);
 
 _axios.interceptors.request.use(
-  function(config) {
-    const {$Progress} = Vue.prototype;
-		if($Progress) {
-			$Progress.start();
-		}
-    return config;
-  },
-  function(error) {
-    // Do something with request error
-    return Promise.reject(error);
-  }
+	function (config) {
+		const { $Progress } = Vue.prototype;
+		if($Progress) $Progress.start();
+
+		return config;
+	},
+	function (error) {
+		// Do something with request error
+		return Promise.reject(error);
+	}
 );
 
 // Add a response interceptor
 _axios.interceptors.response.use(
-  function(response) {
-    const {status, data} = response;
-		const {$toast, $Progress} = Vue.prototype;
-		let errMsg = '';
-		if(status !== 200) {
-			errMsg = `Error code :${status}`;
+	function (response) {
+		const { $Progress, $toast } = Vue.prototype;
+		const {data, status} = response;
+		let msg = "";
+
+		console.log("AJAX", response);
+		if(status != 200) {
+			msg = "서버접속 실패";
 		}
-		if(data.err) {
-			errMsg = data.err;
+		if(data && data.err) {
+			msg = data.err;
 		}
 
-		if(errMsg) {
-			if($toast) $toast.error(errMsg);
+		if(msg) {
+			if($toast) $toast.error(msg);
 			if($Progress) $Progress.fail();
+			console.warn(msg);
 			return false;
 		} else {
 			if($Progress) $Progress.finish();
 			return data;
 		}
-  },
-  function(error) {
-    // Do something with response error
-    return Promise.reject(error);
-  }
+	},
+	function (error) {
+		// Do something with response error
+		return Promise.reject(error);
+	}
 );
 
-const Plugin = {};
-Plugin.install = function(Vue, options) {
-  Vue.axios = _axios;  
-  Object.defineProperties(Vue.prototype, {
-    axios: {
-      get() {
-        return _axios;
-      }
-    },
-    $axios: {
-      get() {
-        return _axios;
-      }
-    },
-  });
-};
+Vue.prototype.$axios = _axios;
 
-Vue.use(Plugin)
-
-export default Plugin;
+export default _axios;
