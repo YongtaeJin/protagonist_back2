512407fa210dbb4bf5da28812f68e4432f308042
Yongtae Jin <freview@.nate.com>
2023-02-23 17:19:20 +0900

    f230223_2

--- src/views/shop/SignedP03Form.vue
+++ src/views/shop/SignedP03Form.vue
@@ -32,6 +32,7 @@
 
 <script>
 import { deepCopy } from "../../../util/lib";
+import {saveAs} from 'file-saver';
 import InputPost3 from '../../components/InputForms/InputPost3.vue';
 export default {
   components: { InputPost3 },
@@ -120,7 +121,52 @@ export default {
     },
   
     async onButtonClick2(item) {  
-     
+      
+      axios({
+            url: "https://protagonist.kro.kr/server/upload/memberPhoto/Hxn3P8Ms5dKepNj7.jpg", // 파일 다운로드 요청 URL
+            method: "GET", // 혹은 'POST'
+            responseType: "blob", // 응답 데이터 타입 정의
+        }).then((response) => {
+            // 다운로드(서버에서 전달 받은 데이터) 받은 바이너리 데이터를 blob으로 변환합니다.
+            const blob = new Blob([response.data]);
+            // 특정 타입을 정의해야 경우에는 옵션을 사용해 MIME 유형을 정의 할 수 있습니다.
+            // const blob = new Blob([this.content], {type: 'text/plain'})
+
+            // blob을 사용해 객체 URL을 생성합니다.
+            const fileObjectUrl = window.URL.createObjectURL(blob);
+
+            // blob 객체 URL을 설정할 링크를 만듭니다.
+            const link = document.createElement("a");
+            link.href = fileObjectUrl;
+            link.style.display = "none";
+
+            // 다운로드 파일 이름을 지정 할 수 있습니다.
+            // 일반적으로 서버에서 전달해준 파일 이름은 응답 Header의 Content-Disposition에 설정됩니다.
+            link.download = extractDownloadFilename(response);
+
+            // 다운로드 파일 이름을 추출하는 함수
+            const extractDownloadFilename = (response) => {
+                const disposition = response.headers["content-disposition"];
+                const fileName = decodeURI(
+                disposition
+                    .match(/filename[^;=\n]*=((['"]).*?\2|[^;\n]*)/)[1]
+                    .replace(/['"]/g, "")
+                );
+                return fileName;
+            };
+
+            // 다운로드 파일의 이름은 직접 지정 할 수 있습니다.
+            // link.download = "sample-file.xlsx";
+
+            // 링크를 body에 추가하고 강제로 click 이벤트를 발생시켜 파일 다운로드를 실행시킵니다.
+            document.body.appendChild(link);
+            link.click();
+            link.remove();
+
+            // 다운로드가 끝난 리소스(객체 URL)를 해제합니다.
+            window.URL.revokeObjectURL(fileObjectUrl);
+        });
+      
     },
 
     
