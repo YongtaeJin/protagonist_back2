9e8c2d6c83148cc5435817bada62778a413d5ae1
Yongtae Jin <freview@.nate.com>
2023-03-08 10:36:26 +0900

    f230308_4

--- src/views/shop/SignedP04Form.vue
+++ src/views/shop/SignedP04Form.vue
@@ -140,7 +140,7 @@ export default {
     },
     
     async onButtonClick2(item) {        
-      const fileName = `https://protagonist.kro.kr:4000${item.t_att}`;
+      const fileName = `https://protagonist.kro.kr${item.t_att}`;
       const downFile = item.n_file2;
       try {
         const response = await fetch(fileName)
@@ -171,7 +171,7 @@ export default {
       }
     },
     async downLoad(item) {
-      const fileName = `https://protagonist.kro.kr:4000${item.t_sample}`;
+      const fileName = `https://protagonist.kro.kr${item.t_sample}`;
       const downFile = item.t_samplefile;
     
       try {
